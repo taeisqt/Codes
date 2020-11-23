b12ba79b7db0ffd9520d32c44f656166b5ee62fc
nishi <nishitagudlavalleti34@gmail.com>
2020-11-23 23:09:57 +0530

    First Pyramid 180 Rotation

---
+++ Half-Pyramid-180.py
@@ -0,0 +1,11 @@
+Print("Please enter a integer")
+b=int(input())
+k=2*b-2
+for i in range(0,b):
+    for j in range(0,k):
+        print(end=" ")
+    k=k-2
+    for j in range(0,i+1):
+        print("* ",end="")
+    print("")
+    
